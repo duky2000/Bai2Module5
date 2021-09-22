50f2c19095eff07efe3c1b3f83f8c86b1e93a97e
duky2000 <truongduky2000@gmail.com>
2021-09-22 11:37:38 +0700

    b2

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,24 @@
-import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
-import { AppModule } from './app/app.module';
-import { environment } from './environments/environment';
-
-if (environment.production) {
-  enableProdMode();
+let sum: number = 0;
+let count: number = 0;
+for (let i = 2; count < 30; i++) {
+  let isPrime: boolean = true;
+  if (i == 2) {
+    sum += i;
+    count++;
+    continue;
+  }
+  for (let j = 2; j <= Math.sqrt(i); j++) {
+    if (i % j == 0) {
+      isPrime = false;
+      break;
+    }
+  }
+  if(!isPrime){
+    continue;
+  }
+  sum += i;
+  count++;
 }
+console.log(sum);
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+
